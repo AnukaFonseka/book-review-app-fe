d5834e54701bc8f645de10d8009af6e6461bba11
AnukaFonseka <akunafonseka@gmail.com>
2024-10-16 19:51:07 +0530

    initialized

---
+++ client/src/home/BannerCard.jsx
@@ -0,0 +1,20 @@
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/effect-cards';
+
+import './styles.css';
+
+// import required modules
+import { EffectCards } from 'swiper/modules';
+
+const BannerCard = () => {
+  return (
+    <div>BannerCard</div>
+  )
+}
+
+export default BannerCard
