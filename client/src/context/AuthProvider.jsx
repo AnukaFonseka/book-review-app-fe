1a57d0d8116f686685db55875789a9a89f3c0883
AnukaFonseka <akunafonseka@gmail.com>
2024-10-22 18:55:33 +0530

    Edited Auth.jsx

--- client/src/context/AuthProvider.jsx
+++ client/src/context/AuthProvider.jsx
@@ -40,7 +40,7 @@ const AuthProvider = ({children}) => {
     const login = async (username, password) => {
         setLoading(true);
         try {
-            const response = await fetch('http://localhost:3000/users/sign-in', {
+            const response = await fetch('http://51.20.82.6:3000/users/sign-in', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
